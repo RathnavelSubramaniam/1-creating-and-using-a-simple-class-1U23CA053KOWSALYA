bd649a3d254d8847242d002afb6ce062689e79f4
1U23CA053KOWSALYA <139530255+1U23CA053KOWSALYA@users.noreply.github.com>
2024-01-20 04:33:56 +0000

    s1

--- Person.java
+++ Person.java
@@ -4,8 +4,7 @@ class Persona{
     String lastName;
     int age;
 
-    Persona(String fName,String lName,int a )
-    {
+    Persona(String fName,String lName,int a ){
         firstName = fName ;
         lastName = lName;
         age = a;
@@ -15,22 +14,19 @@ class Persona{
     {
         return firstName + " "+lastName;
     }
-}
+    }
 class Person{
-public static void main (String args[]){
+      public static void main (String args[]){
 
-    Persona a1 = new Persona ("John","Doe",30);
+     Persona a1 = new Persona ("John","Doe",30);
      Persona a2 = new Persona ("Alice","Smith",25);
 
      String kk = a1.getFullName();
      String lk = a2.getFullName();
      
      System.out.println("Person 1:"+kk);
-    System.out.println("Person 2:"+lk);
+     System.out.println("Person 2:"+lk);
 
-    double average = ((a1.age + a2.age)/2.0);
-    System.out.println("Average Age:" +average);
-
-
-
-  }}
+     double average = ((a1.age + a2.age)/2.0);
+     System.out.println("Average Age:" +average);
+     }}
